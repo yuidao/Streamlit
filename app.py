60d3940ffcfba45909220ae3eb646e73a367647a
yuidao <yuidao2016@gmail.com>
2024-06-26 16:10:25 +0900

    グラフ追加

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title('Hello Streamlit!')
 st.write('This is my first Streamlit app.')
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [10, 20, 25, 30])
+st.pyplot(fig)
